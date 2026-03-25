4bb6edd4029321a2f5f0885f58d0bf8ec49ff6e3
istu56 <githubop94@gmail.com>
2026-03-26 00:14:15 +0530

    Update Youtube.py

--- IstkharMusic/platforms/Youtube.py
+++ IstkharMusic/platforms/Youtube.py
@@ -7,7 +7,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython import VideosSearch
+from py_yt import VideosSearch
 from IstkharMusic.utils.database import is_on_off
 from IstkharMusic.utils.formatters import time_to_seconds
 import os
